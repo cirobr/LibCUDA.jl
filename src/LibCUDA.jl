7b5068b2d14c45315f6b929ee0d79adbecd86a38
cirobr <ciro.rosa@alumni.usp.br>
2026-05-03 08:23:20 -0300

    comment

--- src/LibCUDA.jl
+++ src/LibCUDA.jl
@@ -9,6 +9,7 @@ function cleangpu()
 end
 
 
+# https://discourse.julialang.org/t/garbage-collection-not-aggressive-enough-on-slurm-cluster/61649/10
 function garbage_collection(threshold::Real=0.1)
     @assert (0 <= threshold <= 1) || error("threshold must be in 0:1")
     if rand() < threshold   cleangpu()   end
